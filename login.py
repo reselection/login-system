7933a9f16f5a772784137cc25a5eb5ddaff012ba
reselection <danieldcwverlinden@gmail.com>
2021-02-24 21:58:45 +0100

    added account creation functionality

--- login.py
+++ login.py
@@ -7,11 +7,14 @@ def start_menu():
     print("Welcome to log-database")
     print("(1) Login")
     print("(2) Create account")
+    print("(3) Close application")
     start = input(": ")
     if start == '1':
         login()
     elif start == '2':
         create_account()
+    elif start == '3':
+        quit()
     else:
         print(f"{start} is not a valid option, please try again")
         time.sleep(3)
@@ -21,7 +24,38 @@ def create_account():
     """
     This function will create an account if the user hasn't yet, or wants another.
     """
-    pass
+    conndb = sqlite3.connect('data/login-system.db')
+    cursor = conndb.cursor()
+    try:
+        cursor.execute("CREATE TABLE user_data(user,password)")
+    except:
+        print("Table found")
+        pass
+    print("Type 'quit' to exit")
+    username = input("Enter username: ")
+    if username == 'quit':
+        quit()
+    password = input("Enter password: ")
+    if password == 'quit':
+        quit()
+    
+    cursor.execute("SELECT * FROM user_data")
+    match = cursor.fetchall()
+    
+    for data in match:
+        if data[0] == username:
+            print("Username is taken\nTry again.")
+            time.sleep(3)
+            create_account()
+     
+    
+    cursor.execute("INSERT INTO user_data(user,password) VALUES (?,?)", (username, base64.b64encode(password.encode('utf-8'))))
+
+    conndb.commit()
+    conndb.close()
+    print("Account created to local database.\nReturning to menu...")
+    time.sleep(3)
+    start_menu()
 
 def login(username, password):
     """
